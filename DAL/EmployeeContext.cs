e79ecf8c74e9f89852402fff136f02af7583e353
Karim Ljung <karim.ljung@gmail.com>
2018-12-26 19:27:56 +0100

    Initial commit

---
+++ DAL/EmployeeContext.cs
@@ -0,0 +1,23 @@
+﻿using DomainModel;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL
+{
+    public class EmployeeContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //optionsBuilder.UseSqlServer(
+            //    "Server = (localdb)\\mssqllocaldb; Database = TestDatabase; Trusted_Connection = True;");
+
+            optionsBuilder.UseSqlServer(
+                "Server = .\\SQLEXPRESS; Database = TestDatabase; Trusted_Connection = True;");
+
+            
+            base.OnConfiguring(optionsBuilder);
+        }   
+
+    }
+}
---
+++ DomainModel/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace DomainModel
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+    }
+}
